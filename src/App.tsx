6809ac743500f53e455929c83414b2db58a78e78
cdaringe <cdaringe@gmail.com>
2021-07-05 22:10:47 -0700

    chore: deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import CoinZone from "./CoinZone";
 import Go from "./Go";
 
-const bezos = new Audio("bezos.mp3");
+const BEZOS_URL = "bezos.mp3";
+const bezos = new Audio(BEZOS_URL);
+bezos.controls = true;
 bezos.loop = true;
+bezos.src = BEZOS_URL;
 
 function App() {
   const [isGoTime, setGoTime] = React.useState(false);
   const [isCanPlay, setIsCanPlay] = React.useState(false);
   bezos.addEventListener("canplay", (event) => setIsCanPlay(true));
+  // React.useEffect(() => {
+  //   document.body.appendChild(bezos);
+  //   (window as any).bezos = bezos;
+  // }, []);
   return (
     <div className="App">
       {isGoTime ? (
